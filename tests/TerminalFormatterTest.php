8ac8161b7b8a894213352bfdb60b724c1f05762b
bobmagicii <bob@majdak.net>
2021-01-23 03:41:32 -0600

    basic unit tests for the terminal formatter

---
+++ tests/TerminalFormatterTest.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Nether\Avenue;
+
+use Closure;
+use Nether;
+use PHPUnit;
+
+use Stringable;
+use Nether\Console\TerminalFormatter;
+
+class FormatterErrorCheck
+implements Stringable {
+
+	public String $Expect;
+	public String $Message;
+
+	public function
+	__Construct(String $Expect, String $Message) {
+		$this->Expect = $Expect;
+		$this->Message = $Message;
+		return;
+	}
+
+	public function
+	__ToString():
+	String { return $this->Message; }
+
+	public function
+	__Invoke(String $Result):
+	Bool { return ($Result === $this->Expect); }
+
+};
+
+class TerminalFormatterTest
+extends PHPUnit\Framework\TestCase {
+/*//
+@date 2021-01-23
+//*/
+
+	/** @test */
+	public function
+	TestCheckFormatterErrorCheck() {
+	/*//
+	@date 2021-01-23
+	//*/
+
+		$Bob = new FormatterErrorCheck(
+			'Bob',
+			'could not even say my name'
+		);
+
+		$this->AssertEquals(
+			('Bob'==='Bob'),
+			($Bob('Bob')),
+			((String)$Bob)
+		);
+
+		return;
+	}
+
+	/** @test */
+	public function
+	TestBasicFormatterSequences() {
+	/*//
+	@date 2021-01-23
+	//*/
+
+		$F = new TerminalFormatter;
+		$Method = NULL;
+		$Test = NULL;
+
+		$TestSingle = [
+			'Red' => new FormatterErrorCheck(
+				"\e[31m",
+				'expected \e[31m'
+			),
+			'White_Red' => new FormatterErrorCheck(
+				"\e[97;41m",
+				'expected \e[97;41m'
+			),
+			'BrightWhiteUnderline_Red' => new FormatterErrorCheck(
+				"\e[1;97;4;41m",
+				'expected \e[1;97;4;41m'
+			)
+		];
+
+		$TestWrapped = [
+			'Yellow' => new FormatterErrorCheck(
+				"\e[33mYellow\e[0m",
+				'expected \e[31m'
+			),
+			'Black_Yellow' => new FormatterErrorCheck(
+				"\e[30;43mBlack_Yellow\e[0m",
+				'expected \e[30;43mBlack_Yellow\e[0m'
+			),
+			'DimBlackUnderline_Yellow' => new FormatterErrorCheck(
+				"\e[2;30;4;43mDimBlackUnderline_Yellow\e[0m",
+				'expected \e[2;30;4;43mDimBlackUnderline_Yellow\e[0m'
+			)
+		];
+
+		////////
+
+		// using assert true beacuse assert equals will dump
+		// a diff of the values and break your terminal lol.
+
+		foreach($TestSingle as $Method => $Test)
+		$this->AssertTrue(
+			$Test($F->{$Method}()),
+			"{$Method} {$Test}"
+		);
+
+		foreach($TestWrapped as $Method => $Test)
+		$this->AssertTrue(
+			$Test($F->{$Method}($Method)),
+			"{$Method} {$Test}"
+		);
+
+		return;
+	}
+
+};
